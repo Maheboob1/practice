02b0a2b15167ee94d15f3d495dc069abb77bc974
Maheboob <maheboob.n.tumkur@gmail.com>
2019-08-21 19:07:21 +0530

    updated list of even numbers

--- sorted_list.py
+++ sorted_list.py
@@ -2,7 +2,7 @@ a=[3,2,1,6,5,4,9,8,7]
 b=[]
 for i in a:
     if i not in b:
-        b.append(i)
-print(set(sorted(b)))
+        if i%2==0:
+            b.append(i)
+print((sorted(b)))
 
-#complete program
