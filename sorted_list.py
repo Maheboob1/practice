d27e36ce536b9da0dd30e14ed6d3aaccd0b004f4
Maheboob <maheboob.n.tumkur@gmail.com>
2019-08-22 21:43:00 +0530

    d added

--- sorted_list.py
+++ sorted_list.py
@@ -1,11 +1,15 @@
 a=[3,2,1,6,5,4,9,8,7]
 b=[]
 c=[]
+d=[]
+d=a
 for i in a:
     if i not in b:
         if i%2==0:
             b.append(i)
         else:
             c.append(i)
+
 print((sorted(b)))
 print(sorted(c))
+print(sorted(d))
