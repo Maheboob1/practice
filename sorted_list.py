fc859e38fad67e9503a4cf9dc29232039e659369
Maheboob <maheboob.n.tumkur@gmail.com>
2019-08-22 19:18:25 +0530

    even_odd_sorted_list

--- sorted_list.py
+++ sorted_list.py
@@ -1,8 +1,11 @@
 a=[3,2,1,6,5,4,9,8,7]
 b=[]
+c=[]
 for i in a:
     if i not in b:
         if i%2==0:
             b.append(i)
+        else:
+            c.append(i)
 print((sorted(b)))
-
+print(sorted(c))
