29b65905ccdc28d1be96d21a799b96cc5d3c0282
Maheboob <maheboob.n.tumkur@gmail.com>
2019-08-25 12:29:27 +0530

    dosa burger

--- hungry.py
+++ hungry.py
@@ -1,6 +1,7 @@
 hungry=input("are you hungry? :")
 if hungry=="yes":
     print("Eat something")
-    print("Eat Fast Food")
+    print("Eat Fast Dosa")
+    print("Eat Burger")
 else:
     print("do your work")
